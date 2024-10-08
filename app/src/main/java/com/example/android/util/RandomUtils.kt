b7d87f8cce4e769160cf6ecf9027962e4b68e899
li qishun <414551308@qq.com>
2024-10-08 17:22:17 +0800

    feat:RandomUtils

---
+++ app/src/main/java/com/example/android/util/RandomUtils.kt
@@ -0,0 +1,22 @@
+package com.example.android.util
+
+import java.util.Random
+
+object RandomUtils {
+
+    private val mRandom by lazy { Random() }
+
+    /**
+     * 通过概率判断
+     * 传入30则有30%的概率返回true
+     *
+     * @param prob 概率
+     * */
+    @JvmStatic
+    fun isTrueFromProb(prob: Int): Boolean {
+        val randomNum = mRandom.nextInt(100)
+        return prob > randomNum
+    }
+
+
+}
