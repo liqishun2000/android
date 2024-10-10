b141a5d57805b64b341052ffc26dbc99a770d84f
li qishun <414551308@qq.com>
2024-10-10 16:21:07 +0800

    feat:PermissionActivity

---
+++ app/src/main/java/com/example/android/training/permission/PermissionActivity.kt
@@ -0,0 +1,61 @@
+package com.example.android.training.permission
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.android.ui.theme.AndroidTheme
+
+class PermissionActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AndroidTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "World",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { result ->
+
+            if (result) {
+                Log.d("...", "权限被授予")
+            } else {
+                Log.d("...", "权限被拒绝")
+            }
+        }.also {
+            it.launch(android.Manifest.permission.ACTIVITY_RECOGNITION)
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AndroidTheme {
+        Greeting("Android")
+    }
+}
