bfcf6e4e7680a9de352fbc428808002fa60bbc3f
li qishun <414551308@qq.com>
2024-10-10 18:20:14 +0800

    style:TrainingResultClass

--- app/src/main/java/com/example/android/training/basic/TrainingResultClass.kt
+++ app/src/main/java/com/example/android/training/basic/TrainingResultClass.kt
@@ -1,6 +1,5 @@
 package com.example.android.training.basic
 
-import com.sd.lib.retry.fRetry
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.coroutineScope
