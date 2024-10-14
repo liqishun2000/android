adddd68a4d5d77dc68f4d2a463a813d97ab78195
li qishun <414551308@qq.com>
2024-10-14 10:25:46 +0800

    feat:MainActivity

---
+++ app/src/main/java/com/example/android/ui/MainActivity.kt
@@ -0,0 +1,113 @@
+package com.example.android.ui
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.android.ui.theme.AndroidTheme
+import com.example.android.ui.viewmodel.MainVM
+import com.sd.lib.ctx.fFindActivity
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AndroidTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    MainScreen(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+private fun MainScreen(
+    modifier: Modifier = Modifier,
+    vm: MainVM = viewModel()
+) {
+    val state by vm.stateFlow.collectAsStateWithLifecycle()
+    val context = LocalContext.current
+
+
+    MainView(
+        modifier = modifier,
+        onClickOperate = {
+            context.fFindActivity {
+                vm.click(it)
+            }
+        },
+    )
+}
+
+@Composable
+private fun MainView(
+    modifier: Modifier = Modifier,
+    onClickOperate: () -> Unit = {},
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        Button(onClick = onClickOperate) {
+            Text(text = "点击")
+        }
+
+        Box(
+            modifier = Modifier
+                .background(Color.Gray)
+                .padding(20.dp)
+        ) {
+            Text(text = "content")
+        }
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .aspectRatio(1f)
+                .background(Color.LightGray)
+        ) {
+
+        }
+
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AndroidTheme {
+        MainView()
+    }
+}
--- app/src/main/java/com/example/android/ui/viewmodel/MainVM.kt
+++ app/src/main/java/com/example/android/ui/viewmodel/MainVM.kt
@@ -1,9 +1,15 @@
 package com.example.android.ui.viewmodel
 
+import android.app.Activity
+import android.view.accessibility.AccessibilityManager
+import androidx.activity.ComponentActivity.ACCESSIBILITY_SERVICE
 import com.example.android.ui.viewmodel.base.BaseViewModel
 
 object MainVM:BaseViewModel<MainVM.State,Unit>(State()) {
 
+    fun click(activity: Activity){
+
+    }
 
     fun updateShake(shake: Boolean){
         updateState {
@@ -11,7 +17,12 @@ object MainVM:BaseViewModel<MainVM.State,Unit>(State()) {
         }
     }
 
+    fun updateStepCount(stepCount: Int){
+        updateState { it.copy(stepCount = stepCount) }
+    }
+
     data class State(
-        val shake:Boolean = false
+        val shake:Boolean = false,
+        val stepCount:Int = 0,
     )
 }
