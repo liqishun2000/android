73918b57faeb5b100d0017d89ebd1969d159e1de
liqishun <414551308@qq.com>
2024-11-28 16:29:27 +0800

    feat:InfiniteAnimation

--- app/src/main/java/com/example/android/training/compose/animation/AnimationTraining.kt
+++ app/src/main/java/com/example/android/training/compose/animation/AnimationTraining.kt
@@ -5,7 +5,13 @@ import androidx.compose.animation.EnterTransition
 import androidx.compose.animation.ExitTransition
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
 import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
@@ -17,6 +23,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -27,6 +34,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalDensity
@@ -92,6 +100,79 @@ private fun Test(modifier: Modifier = Modifier) {
 
     }
 }
+
+//region rememberInfiniteTransition 永久循环动画
+@Composable
+fun InfiniteAnimationTraining(modifier: Modifier = Modifier) {
+    var isVisible by remember { mutableStateOf(false) }
+
+    val density = LocalDensity.current
+
+    val infiniteTransition = rememberInfiniteTransition(label = "")
+    val scale by infiniteTransition.animateFloat(
+        initialValue = 1f,
+        targetValue = 1.5f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(1000, easing = LinearEasing),  // 1秒完成一次动画
+            repeatMode = RepeatMode.Reverse  // 反向重复，使动画来回进行
+        ), label = ""
+    )
+
+    val animateState by animateFloatAsState(
+        targetValue = if (isVisible) 0.8f else 1.2f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(durationMillis = 1000),
+            repeatMode = RepeatMode.Reverse
+        ),
+        label = "animateStateTest",
+    )
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        Button(
+            modifier = Modifier.align(Alignment.Center),
+            onClick = { isVisible = !isVisible },
+        ) {
+            Text(text = if (isVisible) "Hide" else "Show")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Box(
+            modifier = Modifier
+                .align(Alignment.Center)
+                .offset(y = 200.dp)
+                .fillMaxWidth()
+                .background(Color.Gray)
+                .height(200.dp),
+            contentAlignment = Alignment.Center
+        ) {
+
+
+            Box(
+                modifier = Modifier
+                    .size(200.dp)
+                    .scale(scale)
+                    .background(Color.Green)
+            ){
+                Box(Modifier.size(50.dp)
+                    .background(Color.Red))
+
+                Text(
+                    text = "Hello, I am visible!",
+                    modifier = Modifier
+                        .padding(16.dp)
+                )
+            }
+
+
+        }
+
+
+    }
+}
+//endregion
 
 //region animate*AsState + graphicsLayer动画
 @Composable
