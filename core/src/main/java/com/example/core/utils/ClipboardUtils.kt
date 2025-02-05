69aa0f469efb9f5585474e7e81fced3ddcbc1e3c
liqishun <414551308@qq.com>
2025-02-05 16:08:45 +0800

    feat:ClipboardUtils

--- core/src/main/java/com/example/core/utils/ClipboardUtils.kt
+++ core/src/main/java/com/example/core/utils/ClipboardUtils.kt
@@ -15,4 +15,19 @@ object ClipboardUtils {
 
         clipboard.setPrimaryClip(clip)
     }
+
+    fun getClipboardContent(context: Context):CharSequence? {
+        // 获取ClipboardManager实例
+        val clipboard = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+
+        // 获取剪贴板内容
+        val clipData: ClipData? = clipboard.primaryClip
+
+        return if (clipData != null && clipData.itemCount > 0) {
+            // 获取剪贴板的第一项并转换为文本
+            clipData.getItemAt(0).text
+        } else {
+            null
+        }
+    }
 }
