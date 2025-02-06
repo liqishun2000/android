04c865821f7bbb6002ca8b83c538b13225166e4b
liqishun <414551308@qq.com>
2025-02-06 11:42:04 +0800

    perf:DashLine

--- core/src/main/java/com/example/core/compose/common/DashLine.kt
+++ core/src/main/java/com/example/core/compose/common/DashLine.kt
@@ -1,13 +1,19 @@
 package com.example.core.compose.common
 
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.PathEffect
+import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
@@ -16,66 +22,57 @@ import androidx.compose.ui.unit.dp
 @Preview
 @Composable
 private fun Preview() {
-    DashedVerticalLine(
-        color = Color.Red,
-        width = 2.dp,
-        modifier = Modifier.height(100.dp)
-    )
+    Column(
+        verticalArrangement = Arrangement.spacedBy(10.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        DashedVerticalLine(
+            color = Color.Red,
+            width = 2.dp,
+            modifier = Modifier.height(100.dp)
+        )
+
+        DashedHorizontalLine(
+            color = Color.Red,
+            width = 2.dp,
+            modifier = Modifier.width(100.dp)
+        )
+    }
+
 }
 
 @Composable
 fun DashedVerticalLine(
-    color:Color,
-    width:Dp,
+    color: Color,
+    width: Dp,
     modifier: Modifier = Modifier
 ) {
-    Canvas(
-        modifier = modifier.width(width = width)
-    ) {
-        val path = Path().apply {
-            // 移动到起始点
-            moveTo(0f, 0f)
-            // 画一条从顶部到底部的直线
-            lineTo(0f, size.height)
-        }
-
-        drawPath(
-            path = path,
+    Canvas(modifier = modifier.width(width)) {
+        drawLine(
             color = color,
-            style = Stroke(
-                width = width.toPx(), // 线条宽度
-                // 定义虚线的模式（例如，每个线段长10dp，间隔5dp）
-                pathEffect = PathEffect.dashPathEffect(floatArrayOf(6.dp.toPx(), 6.dp.toPx()), 0f)
-            )
+            start = Offset(size.width / 2, 0f),
+            end = Offset(size.width / 2, size.height),
+            strokeWidth = width.toPx(),
+            cap = StrokeCap.Round,
+            pathEffect = PathEffect.dashPathEffect(floatArrayOf(6.dp.toPx(), 6.dp.toPx())),
         )
     }
 }
 
 @Composable
 fun DashedHorizontalLine(
-    color:Color,
-    width:Dp,
+    color: Color,
+    width: Dp,
     modifier: Modifier = Modifier
 ) {
-    Canvas(
-        modifier = modifier.height(height = width)
-    ) {
-        val path = Path().apply {
-            val y = width.toPx()/2f
-            // 移动到起始点
-            moveTo(0f, y)
-            // 画一条从顶部到底部的直线
-            lineTo(size.width, y)
-        }
-
-        drawPath(
-            path = path,
+    Canvas(modifier = modifier.height(width)) {
+        drawLine(
             color = color,
-            style = Stroke(
-                width = width.toPx(), // 线条宽度
-                // 定义虚线的模式（例如，每个线段长10dp，间隔5dp）
-                pathEffect = PathEffect.dashPathEffect(floatArrayOf(6.dp.toPx(), 6.dp.toPx()), 0f)
-            )
+            start = Offset(0f, size.height / 2),
+            end = Offset(size.width, size.height / 2),
+            strokeWidth = width.toPx(),
+            cap = StrokeCap.Round,
+            pathEffect = PathEffect.dashPathEffect(floatArrayOf(6.dp.toPx(), 6.dp.toPx())),
         )
     }
 }
