a4f6bca86ee6d9f3d8fed7b798a985b795d4d42d
liqishun <414551308@qq.com>
2025-04-10 10:57:54 +0800

    chore:add explanation

--- core/src/main/java/com/example/core/ktx/NumberKtx.kt
+++ core/src/main/java/com/example/core/ktx/NumberKtx.kt
@@ -3,6 +3,9 @@ package com.example.core.ktx
 import android.content.Context
 import android.util.TypedValue
 
+/**
+ * TypedValue.applyDimension 其他单位转为px 如dp,px
+ * */
 fun Float.dpToPx(context: Context): Int {
     return TypedValue.applyDimension(
         TypedValue.COMPLEX_UNIT_DIP,
