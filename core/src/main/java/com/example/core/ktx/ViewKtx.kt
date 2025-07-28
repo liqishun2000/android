6d0c0fbf15d7fc9df55d6b626afb13b5e70035ed
liqishun <414551308@qq.com>
2025-07-28 10:35:36 +0800

    feat: ViewKtx.kt fullScreen

--- core/src/main/java/com/example/core/ktx/ViewKtx.kt
+++ core/src/main/java/com/example/core/ktx/ViewKtx.kt
@@ -12,6 +12,20 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.core.widget.TextViewCompat
 import kotlin.math.ceil
 
+/**
+ * 沉浸式 悬浮窗沉浸式，还需要WindowManager.LayoutParams设置：
+ *
+ * if (Build.VERSION.SDK_INT >= 28) {
+ *    params.layoutInDisplayCutoutMode =
+ *    WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
+ * }
+ * */
+fun View.fullScreen(){
+    systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+}
+
 fun View.setMargin(
     startDp: Float = 0f,
     endDp: Float = 0f,
