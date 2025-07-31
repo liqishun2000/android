f3c7a4ac610b17ebe2ad47f2e7d530d1e70389e7
liqishun <414551308@qq.com>
2025-07-31 15:35:00 +0800

    feat: EditText note

---
+++ app/src/main/java/com/example/android/training/xml/EditTextTraining.kt
@@ -0,0 +1,11 @@
+package com.example.android.training.xml
+
+/**
+ * xml中设置
+ * android:fitsSystemWindows="true"
+ * 清单文件中设置:
+ * android:windowSoftInputMode="adjustResize|stateVisible"
+ * 可调整窗口大小，为软键盘腾出空间
+ *
+ * EditText默认有点击事件
+ * */
