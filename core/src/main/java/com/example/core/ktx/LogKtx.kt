dc087d3c56ba563e51b5abdc09e44050f463a8f2
liqishun <414551308@qq.com>
2025-08-07 14:34:28 +0800

    perf: log

--- core/src/main/java/com/example/core/ktx/LogKtx.kt
+++ core/src/main/java/com/example/core/ktx/LogKtx.kt
@@ -2,6 +2,25 @@ package com.example.core.ktx
 
 import android.util.Log
 
-fun log(msg: String) = Log.d("...", msg)
+fun log(msg: String) = log("...",msg)
 
-fun logActivityStack(msg:String) = Log.d("activity_stack",msg)
+fun logActivityStack(msg:String) = log("activity_stack",msg)
+
+private fun log(tag:String,msg:String){
+    if (msg.length <= 1000) {
+        Log.d(tag, msg)
+    } else {
+        var currentIndex = 0
+        val list: MutableList<String> = mutableListOf()
+        while (currentIndex < msg.length) {
+            val endIndex = minOf(currentIndex + 1000, msg.length)
+            val substring = msg.substring(currentIndex, endIndex)
+            list.add(substring)
+            currentIndex = endIndex
+        }
+        list.forEach {
+            Log.d(tag, it)
+        }
+        Log.d(tag, "")
+    }
+}
