9812eb5f9e37dfe70a0b5472bbbb0f865b533986
liqishun <414551308@qq.com>
2025-08-15 10:14:56 +0800

    chore: explanation

--- app/src/main/java/com/example/android/training/xml/Training.kt
+++ app/src/main/java/com/example/android/training/xml/Training.kt
@@ -1,2 +1,12 @@
 package com.example.android.training.xml
 
+/**
+ *
+ * 不裁剪子View，子View的子View可以超出绘制
+ * android:clipChildren="false"
+ *
+ * 是否可以在padding区域绘制
+ * android:clipToPadding="false"
+ *
+ *
+ * */
