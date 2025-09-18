8de259c34894f3f728ea3ccf0b024b150da46074
liqishun <414551308@qq.com>
2025-09-18 11:33:55 +0800

    feat: adb shell note

--- app/src/main/java/com/example/android/training/adb/AdbTraining.kt
+++ app/src/main/java/com/example/android/training/adb/AdbTraining.kt
@@ -3,4 +3,8 @@ package com.example.android.training.adb
 /**
  * 传文件
  * adb push {filePath} /storage/emulated/0/fileName
+ * 拿文件 可以拿整个目录，或者一个文件 如果名字含有空格，需要用""
+ * adb pull /storage/emulated/0/DCIM/Screenshots C:\Users\fisrt\AndroidStudioProjects\temp
+ * 列出文件列表
+ * adb shell ls /storage/emulated/0/
  * */
