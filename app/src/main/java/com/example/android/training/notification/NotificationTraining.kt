e2e62a3adf793eb1cf24df77f7dd23c8666a20a0
liqishun <414551308@qq.com>
2025-11-14 11:19:21 +0800

    feat: Notification Note

--- app/src/main/java/com/example/android/training/notification/NotificationTraining.kt
+++ app/src/main/java/com/example/android/training/notification/NotificationTraining.kt
@@ -6,4 +6,6 @@ package com.example.android.training.notification
  *             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT else PendingIntent.FLAG_UPDATE_CURRENT
  *         )
  *  其中reqCode和intent对应，如果reqCode有相同的，那么intent参数会错误
+ *  重启进程后单例会被重置
+ *  切换语言如果重启进程，reqCode可能会有异常
  * */
