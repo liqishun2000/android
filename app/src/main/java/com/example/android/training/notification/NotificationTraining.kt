a9c07dec1ee07bf15d07f4c0bd51b0c1157b5d38
liqishun <414551308@qq.com>
2025-11-14 11:16:42 +0800

    feat: Notification Note

---
+++ app/src/main/java/com/example/android/training/notification/NotificationTraining.kt
@@ -0,0 +1,9 @@
+package com.example.android.training.notification
+
+/**
+ *  PendingIntent.getActivity(
+ *             LibNtySdkImpl.app, reqCode, intent,
+ *             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT else PendingIntent.FLAG_UPDATE_CURRENT
+ *         )
+ *  其中reqCode和intent对应，如果reqCode有相同的，那么intent参数会错误
+ * */
