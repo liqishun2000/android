0f69dcd830dd5923925a003b889c51d26bba4417
liqishun <414551308@qq.com>
2025-12-04 16:36:24 +0800

    NotificationTraining.kt note

--- app/src/main/java/com/example/android/training/notification/NotificationTraining.kt
+++ app/src/main/java/com/example/android/training/notification/NotificationTraining.kt
@@ -8,4 +8,8 @@ package com.example.android.training.notification
  *  其中reqCode和intent对应，如果reqCode有相同的，那么intent参数会错误
  *  重启进程后单例会被重置
  *  切换语言如果重启进程，reqCode可能会有异常
+ *
+ *  intent 的flag要注意，有些手机移除activity栈后需要：
+ *  Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+ *  否则点击通知栏后 activity栈起不来
  * */
