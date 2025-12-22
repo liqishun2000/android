6a4f60ffa482803fa80b23a7bb9a1988900439cb
liqishun <414551308@qq.com>
2025-12-22 17:34:12 +0800

    xml clip note

--- app/src/main/java/com/example/android/training/xml/ClipTrainingActivity.kt
+++ app/src/main/java/com/example/android/training/xml/ClipTrainingActivity.kt
@@ -9,9 +9,15 @@ import com.example.android.databinding.ActivityClipTrainingBinding
  * 不裁剪子View，子View的子View可以超出绘制
  * android:clipChildren="false"
  *
- * 是否可以在padding区域绘制
+ * 该View的子View是否可以在padding区域绘制，
+ * 使用时需注意，如果该View有padding，父View就算设置了
+ * android:clipChildren="false"，子View也不能超出，
+ * 该View一定要设置android:clipToPadding="false".
  * android:clipToPadding="false"
  *
+ * android:clipToOutline="true"
+ * View设置了这个属性，同时背景drawable有圆角，
+ * 那么子View不能超过父View的边界
  *
  * */
 class ClipTrainingActivity: ComponentActivity() {
