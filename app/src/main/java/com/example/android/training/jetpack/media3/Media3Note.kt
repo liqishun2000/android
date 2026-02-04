075b97d988cafcd228a10ad6ed8019d5ab5ab8be
liqishun <414551308@qq.com>
2026-02-04 16:40:35 +0800

    Media3Note.kt

---
+++ app/src/main/java/com/example/android/training/jetpack/media3/Media3Note.kt
@@ -0,0 +1,8 @@
+package com.example.android.training.jetpack.media3
+
+/**
+ * <androidx.media3.ui.PlayerView 点击暂停闪烁问题：
+ * 设置: app:surface_type="texture_view"
+ * app:shutter_background_color="@color/base_transparent"
+ *
+ * */
