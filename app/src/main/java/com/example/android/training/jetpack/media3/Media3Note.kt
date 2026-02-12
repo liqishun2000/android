066e4e24649c351d3095c04ddc48c34894e0c5c3
liqishun <414551308@qq.com>
2026-02-12 17:26:23 +0800

    Media3Note.kt

--- app/src/main/java/com/example/android/training/jetpack/media3/Media3Note.kt
+++ app/src/main/java/com/example/android/training/jetpack/media3/Media3Note.kt
@@ -5,7 +5,7 @@ package com.example.android.training.jetpack.media3
  * 设置: app:surface_type="texture_view"
  * app:shutter_background_color="@color/base_transparent"
  *
- * 媒体通知栏 player事件拦截 用ForwardingPlayer
+ * 媒体通知栏 player事件拦截 用ForwardingPlayer，传给MediaSession
  * class MusicPlayer(
  *     private val player: Player
  * ) : ForwardingPlayer(player) {
@@ -20,4 +20,10 @@ package com.example.android.training.jetpack.media3
  *         AudioPlayer.playNext()
  *     }
  * }
+ * MediaSession.Builder(this, MusicPlayer(exoPlayer!!))
+ *
+ * 自定义媒体会话通知栏:
+ * 通过给CommandButton设置
+ * .setPlayerCommand(Player.COMMAND_SEEK_TO_PREVIOUS)
+ *
  * */
